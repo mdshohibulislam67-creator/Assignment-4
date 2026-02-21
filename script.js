cf2f5b2f83702face23497e4bb8d7ef56452b030
mdshohibulislam67-creator <mdshohibulislam67@gmail.com>
2026-02-22 03:37:13 +0600

    complete UI design

---
+++ script.js
@@ -0,0 +1,8 @@
+let total = document.getElementById('total');
+let totalrejected= document.getElementById('total-rejected');
+let totalinterview = document.getElementById('total-interview');
+const allCardssection = document.getElementById('allCards');
+
+// function calculateCount(){
+
+// }
